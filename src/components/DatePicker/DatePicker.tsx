e68d61611ebed09c8946e99934e2b44aa64818ae
Cody Moore <codymoore240@gmail.com>
2023-08-16 18:40:35 -0400

    initial commit of DatePicker

---
+++ src/components/DatePicker/DatePicker.tsx
@@ -0,0 +1,15 @@
+import { DatePicker as MuiDatePicker, DatePickerProps as MuiDatePickerProps } from '@mui/x-date-pickers/DatePicker'
+/**
+ * A date picker
+ */
+
+type DatePickerProps = {
+}
+
+export const DatePicker = ({
+  
+}: DatePickerProps) => {
+  return (
+    <MuiDatePicker />
+  )
+}
